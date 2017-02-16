1d6ad406b3d11d0056ac507705b6f94089734780
Nicky Heise <nicky.heise@gmail.com>
2017-02-16 10:24:01 +0000

    change design, add animation and add extra button

--- js/main.js
+++ js/main.js
@@ -7,10 +7,26 @@ $(document).ready(function() {
   $(".square").mouseenter(function() {
     $(this).css("background-color", "#7fffd4");
   });
+
+//clears the canvas and resets the color on hover back to #7fffd4//
   $("#clear").click(function() {
+    $("#grid").effect("shake", {times:4}, 1000);
     $(".square").css("background-color", "white");
+    $(".square").mouseenter(function() {
+    $(this).css("background-color", "#7fffd4");
+    });
+  });
+  //creates a random color for each square when button is clicked//
+  $("#randomcolor").click(function() {
+    $(".square").mouseenter(function() {
+    var randomColorChange = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+    $(this).css('background-color', randomColorChange);
+  });
   });
 });
+
+
+
 
 
 
@@ -19,3 +35,4 @@ $(document).ready(function() {
   //var randomColorChange = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
     //$(this).css('background-color', randomColorChange);
     //});
+  //});

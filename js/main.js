fffa6f7848bdb549af6f6ef94995a31e996ec95b
Nicky Heise <nicky.heise@gmail.com>
2017-02-16 21:47:19 +0000

    add functionality for user to choose squares per side of grid

--- js/main.js
+++ js/main.js
@@ -1,38 +1,57 @@
-$(document).ready(function() {
-  for(var i = 0; i < 16; i++) {
-    for(var j = 0; j < 16; j++) {
-    $('#grid').append('<div class= square></div>');
+// function that builds a grid//
+function createGrid(x) {
+    for (var rows = 0; rows < x; rows++) {
+        for (var columns = 0; columns < x; columns++) {
+            $("#grid").append("<div class='square'></div>");
+        };
     };
-  };
-  $(".square").mouseenter(function() {
-    $(this).css("background-color", "#7fffd4");
-  });
+    $(".square").width(480/x);
+    $(".square").height(480/x);
+};
 
-//clears the canvas and resets the color on hover back to #7fffd4//
-  $("#clear").click(function() {
-    $("#grid").effect("shake", {times:4}, 1000);
-    $(".square").css("background-color", "white");
-    $(".square").mouseenter(function() {
+// function that sets the color back to default//
+function set_default_color() {
     $(this).css("background-color", "#7fffd4");
+}
+
+// function that clears the grid
+function clearGrid(){
+    $(".square").remove();
+};
+
+// function that prompts the user to select the number of boxes in a new grid
+// the function then also creates that new grid
+function refreshGrid(){
+    var z = prompt("Please pick number of squares per side up to 60");
+    clearGrid();
+    createGrid(z);
+};
+
+// create a 16x16 grid when the page loads
+// creates a hover effect that changes the color of a square to black when the mouse passes over it, leaving a (pixel) trail through the grid
+// allows the click of a button to prompt the user to create a new grid
+$(document).ready(function() {
+    createGrid(16);
+
+    $(".square").mouseenter(set_default_color);
+
+    //creates a random color for each square when button is clicked//
+    $("#randomcolor").click(function() {
+      $(".square").mouseenter(function() {
+        var randomColorChange = '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
+        $(this).css('background-color', randomColorChange);
+      });
     });
-  });
-  //creates a random color for each square when button is clicked//
-  $("#randomcolor").click(function() {
-    $(".square").mouseenter(function() {
-    var randomColorChange = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-    $(this).css('background-color', randomColorChange);
-  });
-  });
+
+    $("#clear").click(function() {
+    clearGrid();
+    createGrid(16);
+
+      $(".square").mouseenter(set_default_color);
+    });
+
+    $("#usersquares").click(function() {
+          refreshGrid();
+          $(".square").mouseenter(set_default_color);
+    });
 });
-
-
-
-
-
-
-  //Function to create every square produce a random color upon mouse entering.
-  //$(".square").mouseenter(function() {
-  //var randomColorChange = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-    //$(this).css('background-color', randomColorChange);
-    //});
-  //});

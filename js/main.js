ef974387b0920d1b83a19f9351af09b5ca0a049b
Nicky Heise <nicky.heise@gmail.com>
2017-04-27 13:16:46 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -28,7 +28,7 @@ function refreshGrid(){
 };
 
 // create a 16x16 grid when the page loads
-// creates a hover effect that changes the color of a square to black when the mouse passes over it, leaving a (pixel) trail through the grid
+// creates a hover effect that changes the color of a square to mint when the mouse passes over it, leaving a (pixel) trail through the grid
 // allows the click of a button to prompt the user to create a new grid
 $(document).ready(function() {
     createGrid(16);

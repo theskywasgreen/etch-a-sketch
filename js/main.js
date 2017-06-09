811e4fcba0a5726c0d7e042ad680154aa2ee2c55
Nicky Heise <nicky.heise@gmail.com>
2017-06-09 21:57:53 +0100

    add github link

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ function createGrid(x) {
     $(".square").height(480/x);
 };
 
-// function that sets the color back to default//
+// function that sets the color of pixels back to default//
 function set_default_color() {
     $(this).css("background-color", "#7fffd4");
 }
@@ -19,8 +19,8 @@ function clearGrid(){
     $(".square").remove();
 };
 
-// function that prompts the user to select the number of boxes in a new grid
-// the function then also creates that new grid
+// function that prompts the user to select the number of squares in a new grid
+// the function then creates the new grid based on that input
 function refreshGrid(){
     var z = prompt("Please pick number of squares per side up to 60");
     clearGrid();
@@ -28,21 +28,13 @@ function refreshGrid(){
 };
 
 // create a 16x16 grid when the page loads
-// creates a hover effect that changes the color of a square to black when the mouse passes over it, leaving a (pixel) trail through the grid
-// allows the click of a button to prompt the user to create a new grid
+// creates a hover effect that changes the color of a square to #7fffd4 when the mouse passes over it, leaving a pixel trail through the grid
 $(document).ready(function() {
     createGrid(16);
 
     $(".square").mouseenter(set_default_color);
 
-    //creates a random color for each square when button is clicked//
-    $("#randomcolor").click(function() {
-      $(".square").mouseenter(function() {
-        var randomColorChange = '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
-        $(this).css('background-color', randomColorChange);
-      });
-    });
-
+// clears the grid and sets the pixel color back to default #7fffd4 when the button is clicked
     $("#clear").click(function() {
       clearGrid();
       createGrid(16);
@@ -50,16 +42,27 @@ $(document).ready(function() {
     $(".square").mouseenter(set_default_color);
     });
 
+// allows the user to create a grid with pixel size of their preference by clicking button
     $("#usersquares").click(function() {
       refreshGrid();
 
       $(".square").mouseenter(set_default_color);
     });
 
+// creates a random color for each pixel when button is clicked
+    $("#randomcolor").click(function() {
+      $(".square").mouseenter(function() {
+        var randomColorChange = '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
+        $(this).css('background-color', randomColorChange);
+        });
+    });
+
+// allows the user to set pixel color to #7fffd4 when button is clicked
     $("#mint").click(function() {
       $(".square").mouseenter(set_default_color);
     });
 
+// creates a dim effect on the buttons when the user hovers over a button and removes the dim when the cursor is moved away
     $("#clear, #randomcolor, #usersquares, #mint").mouseenter(function() {
       $(this).fadeTo("fast", 0.85);
     });
